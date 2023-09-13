d44fa69cf9cc9817bb62ee61f21b707d1e434ae7
Shubham Acharya <shubham190398@gmail.com>
2023-09-13 14:24:17 +0530

    Adding the constructor for video processor

--- video_process.py
+++ video_process.py
@@ -3,3 +3,18 @@ import numpy as np
 import supervision as sv
 from ultralytics import YOLO
 
+
+class VideoProcessor:
+    def __init__(
+            self,
+            source_weights_path: str,
+            source_video_path: str,
+            target_video_path: str = None,
+            confidence_threshold: float = 0.3,
+            iou_threshold: float = 0.7,
+    ) -> None:
+        self.source_video_path = source_video_path
+        self.target_video_path = target_video_path
+        self.confidence_threshold = confidence_threshold
+        self.iou_threshold = iou_threshold
+        self.model = YOLO(source_weights_path)
