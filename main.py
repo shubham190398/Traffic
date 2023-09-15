d7445f7be9da0e02e6c975b3fb49bd76a3149307
Shubham Acharya <shubham190398@gmail.com>
2023-09-15 15:01:31 +0530

    Adding all the arguments

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ if __name__ == "__main__":
     parser.add_argument(
         "--source_weights_path", required=True, help="Path to source weights file", type=str
     )
+    parser.add_argument(
+        "--source_video_path", required=True, help="Path to source video file", type=str
+    )
+    parser.add_argument(
+        "--target_video_path", default="output.mp4", help="Path to target video", type=str
+    )
+    parser.add_argument(
+        "--confidence_threshold", default=0.3, help="Confidence threshold", type=float
+    )
+    parser.add_argument(
+        "--iou_threshold", default=0.7, help="IOU threshold", type=float
+    )
