94d153b5976a264aaeec715d2f017fd703f401d6
Shubham Acharya <shubham190398@gmail.com>
2023-09-15 14:44:13 +0530

    Adding recorded paths

--- detections_manager.py
+++ detections_manager.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict, List, Set
 import supervision as sv
 import numpy as np
 
@@ -6,6 +6,7 @@ import numpy as np
 class DetectionsManager:
     def __init__(self) -> None:
         self.tracker_id_to_zone_id: Dict[int, int] = {}
+        self.recorded_paths: Dict[int, Dict[int, Set]] = {}
 
     def update(
             self, detections: sv.Detections,
@@ -20,6 +21,9 @@ class DetectionsManager:
             for tracker_id in detections_zone_out.tracker_id:
                 if tracker_id in self.tracker_id_to_zone_id:
                     zone_in_id = self.tracker_id_to_zone_id[tracker_id]
+                    self.recorded_paths.setdefault(zone_out_id, {})
+                    self.recorded_paths[zone_out_id].setdefault(zone_in_id, set())
+                    self.recorded_paths[zone_out_id][zone_in_id].add(tracker_id)
 
         detections.class_id = np.vectorize(lambda x: self.tracker_id_to_zone_id.get(x, -1))(detections.tracker_id)
         detections = detections[detections.class_id != -1]
