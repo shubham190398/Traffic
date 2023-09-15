779058d0c5f13b279dd323db1779553491c8440b
Shubham Acharya <shubham190398@gmail.com>
2023-09-15 14:41:30 +0530

    Adding zone_out logic to update method

--- detections_manager.py
+++ detections_manager.py
@@ -7,10 +7,19 @@ class DetectionsManager:
     def __init__(self) -> None:
         self.tracker_id_to_zone_id: Dict[int, int] = {}
 
-    def update(self, detections: sv.Detections, detections_zones_in: List[sv.Detections]) -> sv.Detections:
+    def update(
+            self, detections: sv.Detections,
+            detections_zones_in: List[sv.Detections],
+            detections_zones_out: List[sv.Detections]
+    ) -> sv.Detections:
         for zone_in_id, detections_zone_in in enumerate(detections_zones_in):
             for tracker_id in detections_zone_in.tracker_id:
                 self.tracker_id_to_zone_id.setdefault(tracker_id, zone_in_id)
+
+        for zone_out_id, detections_zone_out in enumerate(detections_zones_out):
+            for tracker_id in detections_zone_out.tracker_id:
+                if tracker_id in self.tracker_id_to_zone_id:
+                    zone_in_id = self.tracker_id_to_zone_id[tracker_id]
 
         detections.class_id = np.vectorize(lambda x: self.tracker_id_to_zone_id.get(x, -1))(detections.tracker_id)
         detections = detections[detections.class_id != -1]
